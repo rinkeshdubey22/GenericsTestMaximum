85e4b7cfafc98dcdbd0c29c4fe96e1ccaae174cb
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-24 23:31:53 +0530

    [Rinkesh] : add Added Test case class

--- src/main/java/com/bridgelabz/GenericsMaximum.java
+++ src/main/java/com/bridgelabz/GenericsMaximum.java
@@ -1,1 +1,19 @@
+package com.bridgelabz;
 
+public class GenericsMaximum {
+
+    public Integer testMax(Integer x, Integer y, Integer z){
+        int max = x;
+        if(y.compareTo(max) > 0)
+            max = y;
+        if(z.compareTo(max) > 0)
+            max = z;
+        return max;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Generics Maximum Program");
+        GenericsMaximum genericsMaximum = new GenericsMaximum();
+        //System.out.println(genericsMaximum.testMax(3, 4, 5));
+    }
+}
