8cce5be13fd312aa6b677cc8caaf14b3e0c2ecd0
Rinkesh Dubey <rinkeshdubey2@gmail.com>
2021-03-26 13:33:59 +0530

    [Rinkesh] : add Added Generics Method to Find Maximum Number

--- src/main/java/com/bridgelabz/GenericsMaximum.java
+++ src/main/java/com/bridgelabz/GenericsMaximum.java
@@ -2,8 +2,8 @@ package com.bridgelabz;
 
 public class GenericsMaximum {
 
-    public Integer testMaxNumber(Integer x, Integer y, Integer z){
-        int max = x;
+    public  static <T extends Comparable<T>> T testMax(T x, T y, T z){
+        T max = x;
         if(y.compareTo(max) > 0)
             max = y;
         if(z.compareTo(max) > 0)
@@ -11,28 +11,13 @@ public class GenericsMaximum {
         return max;
     }
 
-    public Float testMaxFloat(Float x, Float y, Float z){
-        Float max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-        return max;
-    }
 
-    public String testMaxString(String x, String y, String z){
-        String max = x;
-        if(y.compareTo(max) > 0)
-            max = y;
-        if(z.compareTo(max) > 0)
-            max = z;
-        return max;
-    }
+
+
 
 
     public static void main(String[] args) {
         System.out.println("Welcome to Generics Maximum Program");
-        GenericsMaximum genericsMaximum = new GenericsMaximum();
-        //System.out.println(genericsMaximum.testMax(3.5f, 4.6f, 5.6f));
+        //System.out.println(testMax(4, 3, 7));
     }
 }
--- src/test/java/com/bridgelabz/GenericsMaximumTest.java
+++ src/test/java/com/bridgelabz/GenericsMaximumTest.java
@@ -9,55 +9,55 @@ public class GenericsMaximumTest {
 
     @Test
     public void  givenIntegers_whenFirstIntegerIsMax_ReturnSame() {
-        Integer result = genericsMaximumTest.testMaxNumber(6, 4, 2);
+        Integer result = genericsMaximumTest.testMax(6, 4, 2);
         Assertions.assertEquals(6, result);
     }
 
     @Test
     public void  givenIntegers_whenSecondIntegerIsMax_ReturnSame() {
-        Integer result = genericsMaximumTest.testMaxNumber(6, 7, 2);
+        Integer result = genericsMaximumTest.testMax(6, 7, 2);
         Assertions.assertEquals(7, result);
     }
 
     @Test
     public void  givenInteger_whenThirdIntegerIsMax_ReturnSame() {
-        Integer result = genericsMaximumTest.testMaxNumber(6, 7, 9);
+        Integer result = genericsMaximumTest.testMax(6, 7, 9);
         Assertions.assertEquals(9, result);
     }
 
     @Test
     public void  givenFloat_whenFirstFloatIsMax_ReturnSame() {
-        Float result = genericsMaximumTest.testMaxFloat(6.5f, 4.5f, 2.5f);
+        Float result = genericsMaximumTest.testMax(6.5f, 4.5f, 2.5f);
         Assertions.assertEquals(6.5f, result);
     }
 
     @Test
     public void  givenFloat_whenSecondFloatIsMax_ReturnSame() {
-        Float result = genericsMaximumTest.testMaxFloat(6.5f, 7.5f, 2.5f);
+        Float result = genericsMaximumTest.testMax(6.5f, 7.5f, 2.5f);
         Assertions.assertEquals(7.5f, result);
     }
 
     @Test
     public void  givenFloat_whenThirdFloatIsMax_ReturnSame() {
-        Float result = genericsMaximumTest.testMaxFloat(6.5f, 7.5f, 9.5f);
+        Float result = genericsMaximumTest.testMax(6.5f, 7.5f, 9.5f);
         Assertions.assertEquals(9.5f, result);
     }
 
     @Test
     public void  givenString_whenFirstStringIsMax_ReturnSame() {
-        String result = genericsMaximumTest.testMaxString("Banana", "Apple", "Plum");
+        String result = genericsMaximumTest.testMax("Banana", "Apple", "Plum");
         Assertions.assertEquals("Plum", result);
     }
 
     @Test
     public void  givenString_whenSecondStringIsMax_ReturnSame() {
-        String result = genericsMaximumTest.testMaxString("Apple", "Banana", "Plum");
+        String result = genericsMaximumTest.testMax("Apple", "Banana", "Plum");
         Assertions.assertEquals("Plum", result);
     }
 
     @Test
     public void  givenString_whenThirdStringIsMax_ReturnSame() {
-        String result = genericsMaximumTest.testMaxString("Plum", "Apple", "Banana");
+        String result = genericsMaximumTest.testMax("Plum", "Apple", "Banana");
         Assertions.assertEquals("Plum", result);
     }
 
